3c5381f7598261c06c79ef413fe3c062a3362e25
Everson Teixeira <everson.teixeira@dock.tech>
2024-01-30 15:15:47 -0300

    CORE-1922: Ajustes no server

--- producers/src/main/server.ts
+++ producers/src/main/server.ts
@@ -1,6 +1,9 @@
 import { env } from '@main/config/enviroment'
 import console from 'console'
 import { Job } from 'bull'
+import express, { Express } from 'express'
+
+import { Queue } from 'bull'
 
 interface Message {
   IssuerID: string,
@@ -8,31 +11,53 @@ interface Message {
   Status: string
 }
 
-async function start (): Promise<Job> {
+const app = express()
+app.use(express.json())
+
+async function routes (queue: Queue): Promise<Express> {
+
+  app.get('/queue/send', (req, res) => {
+
+    let data: Message = {
+      IssuerID: '3',
+      FileBase64: 'FileInBase64',
+      Status: 'Pending',
+    }
+
+    // https://github.com/OptimalBits/bull/blob/v4.12.2/REFERENCE.md#queueadd
+    const JobOpts = {
+      removeOnComplete: true, // Remove quando for concluída com sucesso
+      attempts: 100, // Quantidades de tentativas caso falha
+      // delay: 10000, // Tempo em milissegundos para o evento ser processado
+      backoff: { // Configurações para retentativas
+        type: 'fixed',
+        delay: 10000, // Tempo para realizar uma nova tentativa
+      }
+    }
+
+    queue.add(env.bullQueueJobFile, data, JobOpts)
+      .then((job) => {
+        console.log(`QueueName: ${job.queue.name}\nJobID: ${job.id}\nJobName: ${job.name}`)
+        console.log(`IssuerID: ${job.data['IssuerID']}`)
+      })
+      .catch(console.log)
+
+    res.send({ response: 'Mensagem enviada com sucesso!' })
+  })
+
+  return app
+}
+
+async function main () {
   const { setupBull } = await import('@main/bull/client')
   const sendQueue = await setupBull()
   console.log(`Start producer`)
 
-  let message: Message = {
-    IssuerID: '3',
-    FileBase64: 'FileInBase64',
-    Status: 'Pending',
-  }
+  const route = await routes(sendQueue)
 
-  // https://github.com/OptimalBits/bull/blob/v4.12.2/REFERENCE.md#queueadd
-  const JobOpts = {
-    removeOnComplete: true, // Remove quando for concluída com sucesso
-    attempts: 100, // Quantidades de tentativas caso falha
-    // delay: 10000, // Tempo em milissegundos para o evento ser processado
-    backoff: { // Configurações para retentativas
-      type: 'fixed',
-      delay: 10000, // Tempo para realizar uma nova tentativa
-    }
-  }
-  return await sendQueue.add(env.bullQueueJobFile, message, JobOpts)
+  route.listen(3001, () => {
+    console.log(`For the UI, open http://localhost:3001/queue/send`)
+  })
 }
 
-start().then((job) => {
-  console.log(`QueueName: ${job.queue.name}\nJobID: ${job.id}\nJobName: ${job.name}`)
-  console.log(`IssuerID: ${job.data['IssuerID']}`)
-}).catch(console.log)
+main()
